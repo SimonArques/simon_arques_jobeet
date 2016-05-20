72732c25f4e392da1cd19af280147bb3d40a6214
SimonArques <simonarques@gmail.com>
2016-05-20 14:04:08 +0200

    fix loadjobdata

--- src/Ens/SimonBundle/DataFixtures/ORM/LoadJobData.php
+++ src/Ens/SimonBundle/DataFixtures/ORM/LoadJobData.php
@@ -6,7 +6,7 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Ens\SimonBundle\Entity\Job;
-namespace Ens\SimonBundle\DataFixtures\ORM;
+
 
 
 class LoadJobData extends AbstractFixture implements OrderedFixtureInterface
