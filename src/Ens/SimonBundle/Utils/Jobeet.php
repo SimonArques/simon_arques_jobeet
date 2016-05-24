3be22b57635ef80fce7c9e4ef21d0b63341e23fb
SimonArques <simonarques@gmail.com>
2016-05-24 14:02:02 +0200

    2nd test ok

--- src/Ens/SimonBundle/Utils/Jobeet.php
+++ src/Ens/SimonBundle/Utils/Jobeet.php
@@ -3,14 +3,11 @@
 namespace Ens\SimonBundle\Utils;
 class Jobeet
 {
-    static public function slugify($text)
-    {
-        // replace all non letters or digits by -
-        $text = preg_replace('/\W+/', '-', $text);
-
-        // trim and lowercase
-        $text = strtolower(trim($text, '-'));
-
-        return $text;
+     static public function slugify($text)
+     {
+         if (empty($text))
+         {
+             return 'n-a';
+         }
     }
 }
